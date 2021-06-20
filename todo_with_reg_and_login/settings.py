44a776a02a1c40ff83740daf7c2bc0524b0011ac
therealgodwin <godwinlaureto.dev17@gmail.com>
2021-06-20 21:17:05 +0800

    Update settings.py

--- todo_with_reg_and_login/settings.py
+++ todo_with_reg_and_login/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,7 +89,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
